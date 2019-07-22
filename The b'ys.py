870ded5a5695ec9adf6a79a9e8b25b2aca77d9af
Jaden <jaden.m.madison@gmail.com>
2019-07-22 11:12:05 -0700

    added a whole bunch o crap

---
+++ The b'ys.py
@@ -0,0 +1,52 @@
+def main():
+    print("Hollow World")
+    name = input("What is your name kind stranger?")
+    print ("Hello" + name + "! Welcome to the town of samlem.")
+
+def spacer():
+    space=(" ")
+    print(space)
+
+def two():
+    x=10
+    print(x)
+
+def spacer():
+    space=(" ")
+    print(space)
+
+def three():
+    num = input ("I'll guess your number")
+    num = int(num)
+    if num > 10 :
+       print(num + " is greater than 10!")
+    if num < 10 :
+        print ("Boring." + num + " is less than 10.")
+    if num == 10 :
+        print ("WOOT! You are so cool you cose 10!")
+
+def four():
+    oddeven = input("Is your number odd or even?")
+    oddeven = int(oddeven)
+    thatsanoddquestionevenforme = oddeven%2
+    if thatsanoddquestionevenforme == 0
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+    spacer()
+    two()
+    spacer()
+    three()
+    spacer()
+    four()
+    spacer()
+    foive()
+    spacer()
+    six()
+    spacer()
+    sept()
+    spacer()
